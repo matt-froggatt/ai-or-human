eec8d0376566719a97853b049aea86322e2b8ea9
Matthew John Froggatt <mattfr@live.ca>
2021-07-16 20:56:42 -0400

    Serve website and send media link from backend
    Co-authored-by: Yilin Ding <ddddoris1@gmail.com>
    Co-authored-by: William Yang <wywilliamyang9@gmail.com>
    
    Co-authored-by: William Yang <wywilliamyang9@gmail.com>
    Co-authored-by: Yilin Ding <ddddoris1@gmail.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world")
+import express from 'express'
+
+// Load HTTP module
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 8000;
+
+const app = express()
+
+app.get('/media', (req, res) => {
+    res.send("https://i.kym-cdn.com/entries/icons/original/000/030/873/Screenshot_20.jpg")
+  })
+app.use(express.static('build'))
+
+app.listen(port)
