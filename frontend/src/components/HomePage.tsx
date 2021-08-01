004fd55bd081f3529ae712d1a7a8387a533a794b
Matthew John Froggatt <mattfr@live.ca>
2021-08-01 16:43:23 -0400

    Style homepage.
    Co-authored-by: Yilin Ding <ddddoris1@gmail.com>
    Co-authored-by: William Yang <wywilliamyang9@gmail.com>

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -3,10 +3,12 @@ type HomePageProps = {
 }
 
 const HomePage: React.FC<HomePageProps> = (props: HomePageProps) =>
-    <div className="flex flex-col justify-center w-full h-full object-cover" style={{backgroundImage: 'url("https://www.incimages.com/uploaded_files/image/1920x1080/getty_817338718_329077.jpg")'}}>
-        <h1 className="text-center text-5xl text-white"> AI or Not?</h1>  
-        <button name="submit" className="bg-green-400 rounded-md p-2 text-center w-xl" onClick={props.onClick}>Start Game</button>
-        <h2 className="text-center text-3xl text-red-50"> Instructions: Select the artpiece that you think an AI created.</h2>
+    <div className="flex flex-col justify-center items-center w-full h-full bg-center" style={{backgroundImage: 'url("https://www.incimages.com/uploaded_files/image/1920x1080/getty_817338718_329077.jpg")'}}>
+        <div className="flex flex-col justify-center items-center bg-white p-8 rounded-md">
+            <h1 className="text-center text-5xl text-black p-2"> AI or Not?</h1>  
+            <button name="submit" className="bg-green-400 justify-center text-white rounded-full px-6 py-3 text-center w-auto hover:bg-green-600 hover:text-gray-200" onClick={props.onClick}>Start Game</button>
+            <h2 className="text-center text-3xl text-black p-2"> Instructions: Select the artpiece that you think an AI created.</h2>
+        </div>
     </div>
 
 export default HomePage
