6ab2c2e9d02231357c554518727646000508ecb5
Matthew John Froggatt <mattfr@live.ca>
2021-08-02 12:41:24 -0400

    Update game over screen styling
    Co-authored-by: Yilin Ding <ddddoris1@gmail.com>
    Co-authored-by: William Yang <wywilliamyang9@gmail.com>

--- frontend/src/components/GameOver.tsx
+++ frontend/src/components/GameOver.tsx
@@ -4,12 +4,10 @@ type GameOverProps = {
 }
 
 const GameOver: React.FC<GameOverProps> = (props: GameOverProps) =>
-    <div className="flex flex-col justify-center items-center h-full w-full bg-yellow-900 bg-center"  style={{backgroundImage: 'url("https://www.incimages.com/uploaded_files/image/1920x1080/getty_817338718_329077.jpg")'}}>
-        <div className="flex flex-col justify-center items-center p-8 bg-white rounded-md">
-            <h1 className="text-5xl"> GAME OVER</h1>
-            <p className="text-xl"> Your score is: {props.score}</p>
-            <button name="submit" className="mt-4 bg-green-400 justify-center text-white rounded-full px-6 py-3 text-center w-auto hover:bg-green-600 hover:text-gray-200" onClick={props.onClick}>Play Again</button>
-        </div>
+    <div className="flex flex-col justify-center items-center h-full w-full bg-black font-pixel text-red-700" >
+        <h1 className="text-5xl"> GAME OVER</h1>
+        <p className="text-xl"> Your score is: {props.score}</p>
+        <button name="submit" className="mt-4 border-2 border-green-600 justify-center text-green-600 rounded-full px-6 py-3 text-center w-auto hover:bg-green-600 hover:text-gray-200" onClick={props.onClick}>Play Again</button>
     </div>
 
 export default GameOver
